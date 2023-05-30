d40e6c48373573f3728de44f4e95c9a079a31c42
NineSirius <ruslanbulah3@gmail.com>
2023-05-30 22:13:06 +0600

    Added routing and material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { Route, Routes } from 'react-router'
+import { HomePage } from './containers/HomePage'
+
 function App() {
-  return (
-    <>
-      <div>
-        <h1>hello</h1>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+            </Routes>
+        </>
+    )
 }
 
-export default App;
+export default App
